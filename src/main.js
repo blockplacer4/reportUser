85464045f7c3bbb0502ed62a27de51bf4547587b
NichtDerDa <145838215+NichtDerDa@users.noreply.github.com>
2025-08-15 20:51:27 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ export default async function ({req, res, log, error}) {
     
     log("test");
     client
-        .setEndpoint(process.env.ENDPOINT)
+        .setEndpoint(process.env.ENDPOINT.trim())
         .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
         .setKey(process.env.APPWRITE_API_KEY);
     const databases = new Databases(client);
